72589f0d08bc2a1a9ea69359cac760dfa577929c
alcor <2336107524@qq.com>
2023-08-20 15:24:17 +0800

    8/20三号视频完成

--- Project1/Tetris.cpp
+++ Project1/Tetris.cpp
@@ -6,6 +6,7 @@ Tetris::Tetris(int rows, int cols, int left, int right, int top, int blocksize)
 
 void Tetris::init()
 {
+	delay = 30;//这里修改延时
 }
 
 void Tetris::play()
@@ -21,9 +22,33 @@ void Tetris::play()
 
 		//控制运行间隔
 		timelast += getdelay();
-		//渲染游戏画面
-		updatewindow();
+
+		if (timelast >= delay)
+		{
+			timelast = 0;
+			drop();
+			update = true;
+		} 
+
+		if (update)//为什么这里要用一个update来判断？
+		{
+			update = false;//经典PV互斥的思想，能保证不会并发运行，且可以在其他地方设置通过条件
+			//如此就能把单一的条件扩大。从单判定变成多判定，那么以后在不能并发运行的程序时，就应该采用这种做法
+			updatewindow();//渲染游戏画面
+
+			//考虑游戏清行
+			cleanline();
+		}
+		
 	}
+}
+
+void Tetris::drop()
+{
+}
+
+void Tetris::cleanline()
+{
 }
 
 void Tetris::keyevent()
--- Project1/Tetris.h
+++ Project1/Tetris.h
@@ -6,10 +6,14 @@ public:
     Tetris(int rows,int cols,int left,int right,int top,int blocksize);//形参是一种接口
 	void init();
 	void play();
+	void drop();
+	void cleanline();
 
 private:
 	void keyevent();
 	void updatewindow();
 	int getdelay();//返回距离上一次调用函数的时间
+	int delay;//定义延时
+	bool update;//是否更新
 };
 
